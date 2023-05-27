f27110e3237d30d96d49b6cd68a53b1cef393ec4
Murilo <murilotimo@gmail.com>
2023-05-27 02:23:27 -0300

    Finalisando o Backend Em Python

--- container-python.dockerfile
+++ container-python.dockerfile
@@ -1,11 +1,17 @@
 # Use a imagem base do Python 3.11
 FROM python:3.11
 
+# Define o diretório de trabalho dentro do contêiner
+WORKDIR /app
+
 # Instala as dependências do projeto
-RUN pip install flask pyodbc
+RUN pip install flask pymssql
 
 # Expõe a porta 5000 para acesso à API
 EXPOSE 5000
 
 # Define o comando padrão para executar a aplicação Flask
-CMD ["python", "app.py"]
+#CMD ["python", "app.py"]
+
+#inicia o servidor flask em modo de desenvolvimento 
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5000", "--reload"]
--- srcPython/app.py
+++ srcPython/app.py
@@ -1,10 +1,220 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pymssql
+from decimal import Decimal, ROUND_HALF_UP
 
 app = Flask(__name__)
 
+# Configurar a conexão com o banco de dados SQL Server
+server = 'dbhackathon.database.windows.net'
+port = 1433
+database = 'hack'
+username = 'hack'
+password = 'Password23'
+
+# Configurar a conexão com o banco de dados MySQL
+conn = pymssql.connect(
+    host=server,
+    port=port,
+    user=username,
+    password=password,
+    database=database
+)
+
+# Gera sugestões baseadas nos parametros informados pelos usuários que não encontraram produtos adequados
+def gera_sugestao(valor_desejado, prazo):
+    sugestoes = []
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT min(VR_MINIMO) from hack.dbo.PRODUTO')
+    valor_minimo = cursor.fetchone()[0]
+
+    if valor_desejado < valor_minimo:
+        sugestoes.append(f'O valor mínimo de empréstimo é de 200 reais R$ {valor_minimo}')
+
+    sql_produto_por_valor = f'''
+        SELECT
+        CO_PRODUTO,
+        NO_PRODUTO,
+        PC_TAXA_JUROS,
+        NU_MINIMO_MESES,
+        NU_MAXIMO_MESES,
+        VR_MINIMO,
+        VR_MAXIMO
+    FROM
+        hack.dbo.PRODUTO
+    WHERE
+            VR_MINIMO <= {valor_desejado} 
+        AND (VR_MAXIMO >= {valor_desejado} OR VR_MAXIMO IS NULL);
+    '''
+
+    cursor.execute(sql_produto_por_valor)
+    produtos_por_valor = cursor.fetchall()
+
+    for produto in produtos_por_valor:
+        msg = f'Para o valor de R$ {valor_desejado} você pode contratar o produto {produto[1]} com taxa de juros de { round( produto[2]*100 , 2) }% ao mês e prazo de {produto[3]} a {produto[4]} meses.'
+        sugestoes.append(msg)
+
+    envelope_retorno = {'error': 'Nenhum produto encontrado para os valores e prazos informados.',
+                        'sugestoes': sugestoes}
+    return jsonify(envelope_retorno)
+
+# calcula a simulacao das parcelas para o sistema de amortização SAC
+def calcular_amortizacao_sac(valor_desejado, prazo, taxa_juros):
+    # Implementando a lógica de cálculo do SAC
+    amortizacao = Decimal(valor_desejado) / prazo
+    saldo_devedor = Decimal(valor_desejado)
+    taxa_juros = Decimal(taxa_juros)
+    prestacoes = []
+
+    for mes in range(1, prazo + 1):
+        juros = saldo_devedor * taxa_juros
+        prestacao = amortizacao + juros
+        saldo_devedor -= amortizacao
+
+        prestacao = prestacao.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        amortizacao = amortizacao.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        juros = juros.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+
+        prestacoes.append({
+            'numero': mes,
+            'valorAmortizacao': amortizacao,
+            'valorJuros': juros,
+            'valorPrestacao': prestacao,
+        })
+
+    return prestacoes
+
+# calcula a simulacao das parcelas para o sistema de amortização PRICE
+def calcular_amortizacao_price(valor_desejado, prazo, taxa_juros):
+    valor_desejado = Decimal(valor_desejado)
+    prazo = int(prazo)
+    parcelas = []
+
+    taxa_juros_mensal = taxa_juros
+    coeficiente = (taxa_juros_mensal * (1 + taxa_juros_mensal) ** prazo) / ((1 + taxa_juros_mensal) ** prazo - 1)
+    prestacao = valor_desejado * coeficiente
+
+    saldo_devedor = valor_desejado
+
+    for mes in range(1, prazo + 1):
+        juros = saldo_devedor * taxa_juros_mensal
+        amortizacao = prestacao - juros
+        saldo_devedor -= amortizacao
+
+        prestacao = prestacao.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        amortizacao = amortizacao.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        juros = juros.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        saldo_devedor = saldo_devedor.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+
+        parcelas.append({
+            'numero': mes,
+            'valorAmortizacao': amortizacao,
+            'valorJuros': juros,
+            'valorPrestacao': prestacao,
+        })
+
+    return parcelas
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    sql = '''
+    SELECT
+	CO_PRODUTO,
+	NO_PRODUTO,
+	PC_TAXA_JUROS,
+	NU_MINIMO_MESES,
+	NU_MAXIMO_MESES,
+	VR_MINIMO,
+	VR_MAXIMO
+FROM
+	hack.dbo.PRODUTO;
+    '''
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+   
+    return jsonify(rows)
+
+# Definir a rota para receber a solicitação de simulação de empréstimo
+@app.route('/simulacao', methods=['POST'])
+def simulacao_emprestimo():
+    envelope_json = request.get_json()
+
+    # Verificar se todos os campos obrigatórios estão presentes
+    if 'prazo' not in envelope_json or 'valorDesejado' not in envelope_json:
+        return jsonify({'error': 'É necessário informar o prazo e o valor desejado'})
+
+    prazo = envelope_json['prazo']
+    valor_desejado = envelope_json['valorDesejado']
+
+    # Validar o tipo de dados dos campos
+    if not isinstance(prazo, int) or not isinstance(valor_desejado, (int, float)):
+        return jsonify({'error': 'Os valores de prazo e valor desejado devem ser números'})
+
+    # Verificar se os valores estão dentro das faixas permitidas
+    if prazo <= 0 or valor_desejado <= 0:
+        return jsonify({'error': 'Valores devem ser maiores que zero'})
+
+    # Consultar informações parametrizadas no banco de dados
+    cursor = conn.cursor()
+    sql_selecionar_produto = f'''
+    SELECT
+        CO_PRODUTO,
+        NO_PRODUTO,
+        PC_TAXA_JUROS,
+        NU_MINIMO_MESES,
+        NU_MAXIMO_MESES,
+        VR_MINIMO,
+        VR_MAXIMO
+    FROM
+        hack.dbo.PRODUTO
+    WHERE
+            NU_MINIMO_MESES <= {prazo} 
+        AND (NU_MAXIMO_MESES >= {prazo} OR NU_MAXIMO_MESES IS NULL)
+        AND VR_MINIMO <= {valor_desejado}
+        AND (VR_MAXIMO >= {valor_desejado} OR VR_MAXIMO IS NULL);
+    '''
+
+    cursor.execute(sql_selecionar_produto)
+    produtos = cursor.fetchall()
+
+    # Verificar se algum produto foi encontrado para os parâmetros informados
+    if not produtos:
+        return gera_sugestao(valor_desejado,prazo)
+    # Verificar se mais de um produto foi encontrado para os parâmetros informados
+    elif len(produtos) > 1:
+        return jsonify({'error': 'Mais de um produto encontrado para os valores e prazos informados.'})
+    # Se apenas um produto foi encontrado, continuar com a simulação
+    else:       
+        # Realizar os cálculos para os sistemas de amortização SAC e PRICE
+        resultado_sac = calcular_amortizacao_sac(valor_desejado,prazo,produtos[0][2])
+        resultado_price = calcular_amortizacao_price(valor_desejado,prazo,produtos[0][2])
+
+        # Retornar o envelope JSON com o nome do produto e os resultados da simulação
+        envelope_retorno = {
+            'codigoProduto': produtos[0][0],
+            'descricaoProduto': produtos[0][1],
+            'taxaJuros': produtos[0][2],
+            'resultadoSimulacao': [
+                {
+                    'tipo': 'SAC',
+                    'parcelas': resultado_sac
+                },
+                {
+                    'tipo': 'PRICE',
+                    'parcelas': resultado_price
+                }
+            ]
+        }
+
+        # Gravar o envelope JSON no Eventhub
+        #gravar_eventhub(envelope_retorno)
+
+        return jsonify(envelope_retorno)
+
+def validar_dados(envelope_json, parametro):
+    # Implementar a lógica de validação dos dados
+    return True  # Substitua pela sua própria lógica
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
