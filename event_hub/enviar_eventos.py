d4b1ea24fdb2d47e32c1079e53120c8293ac51f5
Murilo <murilotimo@gmail.com>
2023-05-28 01:52:43 -0300

    Teste Envio e Recebimento de mensagensdo Azure Event Hub

---
+++ event_hub/enviar_eventos.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from azure.eventhub import EventData
+from azure.eventhub.aio import EventHubProducerClient
+
+EVENT_HUB_CONNECTION_STR = "Endpoint=sb://meuhackcaixa.servicebus.windows.net/;SharedAccessKeyName=minha-politica;SharedAccessKey=kpOB1ZLepFAnb3evFzjspxCXX+voi6Inn+AEhHZBSlA=;EntityPath=testehackcaixa"
+
+async def run():
+    # Create a producer client to send messages to the event hub.
+    # Specify a connection string to your event hubs namespace and
+    # the event hub name.
+    producer = EventHubProducerClient.from_connection_string(
+        conn_str=EVENT_HUB_CONNECTION_STR
+    )
+    async with producer:
+        # Create a batch.
+        event_data_batch = await producer.create_batch()
+
+        # Add events to the batch.
+        event_data_batch.add(EventData("First event "))
+        event_data_batch.add(EventData("Second event"))
+        event_data_batch.add(EventData("Third event"))
+
+        # Send the batch of events to the event hub.
+        await producer.send_batch(event_data_batch)
+
+asyncio.run(run())    
---
+++ event_hub/receber_eventos.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from azure.eventhub.aio import EventHubConsumerClient
+from azure.eventhub.extensions.checkpointstoreblobaio import (
+    BlobCheckpointStore,
+)
+
+EVENT_HUB_CONNECTION_STR = "Endpoint=sb://meuhackcaixa.servicebus.windows.net/;SharedAccessKeyName=minha-politica;SharedAccessKey=kpOB1ZLepFAnb3evFzjspxCXX+voi6Inn+AEhHZBSlA=;EntityPath=testehackcaixa"
+
+async def on_event(partition_context, event):
+    # Print the event data.
+    print(
+        'Received the event: "{}" from the partition with ID: "{}"'.format(
+            event.body_as_str(encoding="UTF-8"), partition_context.partition_id
+        )
+    )
+
+    # Update the checkpoint so that the program doesn't read the events
+    # that it has already read when you run it next time.
+    await partition_context.update_checkpoint(event)
+
+async def main():
+    # Create a consumer client for the event hub.
+    client = EventHubConsumerClient.from_connection_string(
+        EVENT_HUB_CONNECTION_STR,
+        consumer_group="$Default"
+    )
+    async with client:
+        # Call the receive method. Read from the beginning of the
+        # partition (starting_position: "-1")
+        await client.receive(on_event=on_event, starting_position="-1")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    # Run the main method.
+    loop.run_until_complete(main())
